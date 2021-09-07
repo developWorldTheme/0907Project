203237fc4552f9987b0abd1635856fabe6d60b45
Dongwon92 <canyon920@gmail.com>
2021-09-07 17:35:08 +0900

    Initial commit

---
+++ src/main/java/com/example/proejct0907/Application.java
@@ -0,0 +1,13 @@
+package com.example.proejct0907;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
